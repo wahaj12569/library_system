4276ac26bd7cc6f9a490df639ffa174722162953
wahaj12569 <wahaj12569@gmail.com>
2026-01-02 19:38:26 +0500

    error solve for vercel deployment

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 import os
-load_dotenv()
+
 
 class Settings:
     DB_HOST: str = os.getenv("DB_HOST")
